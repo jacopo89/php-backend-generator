e2140992e04212fa480435c250310169d63b7ba1
jacopo <jacopotrapani89@gmail.com>
2021-04-23 07:53:26 +0200

    extension test

--- src/DependencyInjection/BackendGeneratorExtension.php
+++ src/DependencyInjection/BackendGeneratorExtension.php
@@ -4,14 +4,28 @@
 namespace BackendGenerator\Bundle\BackendGeneratorBundle\DependencyInjection;
 
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class BackendGeneratorExtension extends Extension
 {
 
     public function load(array $configs, ContainerBuilder $container)
     {
-        // TODO: Implement load() method.
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yaml');
+        $loader->load('routes.yaml');
+
+        $packageLoader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config/packages')
+        );
+
+        $packageLoader->load("api_platform.yaml");
     }
 }
