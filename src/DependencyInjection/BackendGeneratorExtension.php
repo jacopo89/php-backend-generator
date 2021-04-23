ce5a444f62e9adc1f4106fffe3fcb2be0618409d
jacopo <jacopotrapani89@gmail.com>
2021-04-23 07:23:17 +0200

    configs

---
+++ src/DependencyInjection/BackendGeneratorExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace BackendGenerator\Bundle\BackendGeneratorBundle\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class BackendGeneratorExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}
